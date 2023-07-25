925505ba45d7862f6cdb99ad71a0f457e6a1f507
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-07-24 17:50:36 -0700

    Create Music-Artists.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Music-Artists.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Music_Artists` (Artist_ID INT64,
+Entity_Type STRING,
+Artist_Type STRING,
+Artist_Name STRING,
+Artist_Picture STRING,
+Artist_Bio STRING,
+Artist_Genre STRING,
+Num_Songs INT64,
+Num_Albums INT64,
+NUm_Awards INT64,
+Updated_By STRING,
+Updated_At Date);
