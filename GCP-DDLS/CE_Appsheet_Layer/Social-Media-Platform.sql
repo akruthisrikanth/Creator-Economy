eb5ff31155b96ea92a0dddee64b4ad5c4bc233a1
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-07-24 17:50:56 -0700

    Create Social-Media-Platform.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Social-Media-Platform.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Social_Media_Platform` (Social_Media_Platform_ID INT64,
+Social_Media_Plstform STRING,
+Logo STRING,
+Details STRING,
+Updated_By STRING,
+Updated_At Date);
