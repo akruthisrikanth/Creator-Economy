1ff5bdc8633d72391460e73be7721dde6f5f4af3
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-07-24 17:51:33 -0700

    Create Trends.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Trends.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Trends` (Trend_ID INT64,
+Trend_Name STRING,
+Trend_Start_Date DATE,
+Num_Impressions INT64,
+Num_Conversations INT64,
+Num_Videos INT64,
+Updated_By STRING,
+Updated_At Date);
