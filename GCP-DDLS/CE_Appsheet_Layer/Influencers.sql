9da4dc054a336af7318c76564aa4c64cd7387f86
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-07-24 17:50:18 -0700

    Create Influencers.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Influencers.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Influencers` (Influencer_ID INT64,
+Influencer_Type STRING,
+Influencer_Name STRING,
+Estimated_Annual_Revenue INT64,
+Trends STRING,
+Updated_By STRING,
+Updated_At Date);
