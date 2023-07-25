a8c46c8e0c80365cb8c8bc9aaf45f42a0e038a24
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-07-24 17:44:24 -0700

    Create Creator-TikTok.sql

---
+++ GCP-DDLS/CE-Datahub/Creator-TikTok.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE TABLE `CE_Datahub.CE_TikTok_Snapshot` AS
+SELECT 
+B.Creator_ID, 
+C.Artist_ID,
+B.Creator_Name,
+" " AS TikTok_ID,
+A.TikTok_followers AS TikTok_Followers,
+A.TikTok_Genz_followers,
+NULL AS TikTok_Average_Views,
+A.TikTok_total_views_for_top_n_tracks AS TikTok_Views_Top_Tracks,
+A.TikTok_total_creations_for_top_n_tracks AS TikTok_Creations_Top_Tracks,
+NULL AS TikTok_Videos,
+NULL AS TikTok_Shares,
+A.TikTok_avg_engagements_per_post AS TikTok_Avg_Engagement_per_Post
+FROM
+`CE_External_Source.Chartmetric_Raw` A
+LEFT JOIN `CE_Datahub.Creator_Artist_Map` C
+ON A.artist_id = C.Artist_ID
+LEFT JOIN `CE_Datahub.CE_Creators` B
+ON C.Creator_ID = B.Creator_ID;
+-- ON A.name = B.Creator_Name;
+
+CREATE OR REPLACE TABLE `CE_Datahub.CE_TikTok_Activity` AS
+SELECT 
+B.Creator_ID, 
+C.Artist_ID,
+B.Creator_Name,
+" " AS TikTok_ID,
+A.TikTok_followers AS TikTok_Followers,
+A.TikTok_Genz_followers,
+NULL AS TikTok_Average_Views,
+A.TikTok_total_views_for_top_n_tracks AS TikTok_Views_Top_Tracks,
+A.TikTok_total_creations_for_top_n_tracks AS TikTok_Creations_Top_Tracks,
+NULL AS TikTok_Videos,
+NULL AS TikTok_Shares,
+A.TikTok_avg_engagements_per_post AS TikTok_Avg_Engagement_per_Post,
+A.Snapshot_Date AS Date
+FROM
+`CE_External_Source.Chartmetric_Raw` A
+LEFT JOIN `CE_Datahub.Creator_Artist_Map` C
+ON A.artist_id = C.Artist_ID
+LEFT JOIN `CE_Datahub.CE_Creators` B
+ON C.Creator_ID = B.Creator_ID;
