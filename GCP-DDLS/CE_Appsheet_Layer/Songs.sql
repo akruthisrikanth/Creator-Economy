0950ff8c31908cdfb3102d557052042d4e63ebd1
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-08-03 19:49:17 -0700

    Create Songs.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Songs.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Songs`
+(
+  Creator_ID STRING,
+  Artist_ID STRING,
+  Artist_Name STRING,
+  Album_ID STRING,
+  Album_Name STRING,
+  Song_ID STRING,
+  Song_Name STRING,
+  Song_Duration INT64,
+  Song_Genre STRING,
+  Album_Artwork STRING,
+  Featuring STRING,
+  Song_Content STRING,
+  Song_Url STRING,
+  Updated_By STRING,
+  Updated_At DATETIME
+);
