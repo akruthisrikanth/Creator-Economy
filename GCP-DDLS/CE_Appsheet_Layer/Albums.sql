648a3d98c1dbadf0e2ee175219d05729fbdad8cf
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-08-03 19:42:20 -0700

    Create Albums.sql

---
+++ GCP-DDLS/CE_Appsheet_Layer/Albums.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE `CE_Appsheet_Layer.Albums`
+(
+  Creator_ID STRING,
+  Artist_ID STRING,
+  Artist_Name STRING,
+  Album_ID STRING,
+  Album_Name STRING,
+  Album_Artwork STRING,
+  Music_Publisher STRING,
+  Release_Date DATE,
+  Collaborating_With STRING
+);
