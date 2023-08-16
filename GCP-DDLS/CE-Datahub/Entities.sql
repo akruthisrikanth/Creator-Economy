539478391091acc2e8a76c29949ee2897bafd8e5
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-08-15 21:51:25 -0700

    Delete Entities.sql

--- GCP-DDLS/CE-Datahub/Entities.sql
+++
@@ -1,8 +0,0 @@
--- INSERT into `CE_Datahub.CE_Creators` SELECT GENERATE_UUID(), Name, FALSE, TRUE FROM `CE_External_Source.Chartmetric_Raw`; 
-
-CREATE OR REPLACE TABLE `CE_Datahub.CE_Entities`(Entity_ID STRING, Entity_Name STRING, Entity_Type STRING);
-
-INSERT INTO `CE_Datahub.CE_Entities` VALUES(Generate_UUID(), 'Creators', " ");
-INSERT INTO `CE_Datahub.CE_Entities` VALUES(Generate_UUID(), 'Trends', " ");
-INSERT INTO `CE_Datahub.CE_Entities` VALUES(Generate_UUID(), 'Brands', " ");
-INSERT INTO `CE_Datahub.CE_Entities` VALUES(Generate_UUID(), 'Content', " ");
