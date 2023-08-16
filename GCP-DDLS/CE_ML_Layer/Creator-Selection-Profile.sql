a2ad691a9b760871bb4b1d3bb197c3331c085ccd
akruthi99 <75107340+akruthi99@users.noreply.github.com>
2023-08-16 12:24:05 -0700

    Update Creator-Selection-Profile.sql

--- GCP-DDLS/CE_ML_Layer/Creator-Selection-Profile.sql
+++ GCP-DDLS/CE_ML_Layer/Creator-Selection-Profile.sql
@@ -8,7 +8,10 @@ A.country_code AS Country_Code,
 [STRUCT(A.twitter_followers, A.spotify_followers, A.instagram_followers, A.youTube_subscribers, A.tikTok_followers, A.Instagram_Genz_followers, A.TikTok_Genz_followers, A.Youtube_Genz_subscribers)] AS Audience,
 [STRUCT(A.twitter_retweets, A.youtube_monthly_video_views, A.spotify_monthly_listeners, A.TikTok_total_views_for_top_n_tracks)] AS Content_Performance,
 [STRUCT(A.Instagram_avg_engagements_per_post, A.TikTok_avg_engagements_per_post, A.Youtube_avg_engagements_per_post, A.facebook_likes, A.facebook_talks)] AS Engagement,
-A.TikTok_total_creations_for_top_n_tracks,
+A.TikTok_total_creations_for_top_n_tracks AS TikTok_total_creations_for_top_n_tracks,
+A.TikTok_Genz_followers_percentage,
+A.Instagram_Genz_followers_percentage,
+A.Youtube_Genz_subscribers_percentage,
 B.Z_Score
 FROM
 `tech-cali-b2c.CE_Analytics_Layer.Creator_Social_Profile` A
